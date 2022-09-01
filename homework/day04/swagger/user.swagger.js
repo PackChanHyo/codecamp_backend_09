ff5e18a7d539cc767fede17d65b8294cfc4c9713
PackChanHyo <cksgy1510@gmail.com>
2022-09-01 21:59:03 +0900

    day04_Api

---
+++ homework/day04/swagger/user.swagger.js
@@ -0,0 +1,35 @@
+/**
+ *  @swagger
+ *  /users:
+ *      get:
+ *          summary : 회원목록 가져오기
+ *          tags: [Boards]
+ *          parmeters:
+ *              - in:query
+ *                  name:number
+ *                  type:int
+ *          responses:
+ *              200:
+ *                  description: 성공
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: array
+ *                              items:
+ *                                  properties:
+ *                                      email:
+ *                                          type: string
+ *                                          example: aaa@gmail.com
+ *                                      name:
+ *                                          type: string
+ *                                          example: 철수
+ *                                      phone:
+ *                                          type: string
+ *                                          example: 010-1234-5678
+ *                                      personal:
+ *                                          type: string
+ *                                          example: 220110-0000000
+ *                                      prefer:
+ *                                          type: string
+ *                                          example: https://naver.com
+ */
